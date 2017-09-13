5175512f0bcb9fa573c1a84c516769f289bd1280
MJFoster <maryjofoster@u.boisestate.edu>
2017-09-12 22:45:37 -0600

    Added Higher Order Function Examples.

--- Udemy/WebDevBootcamp/JS Functions/JS_Functions.js
+++ Udemy/WebDevBootcamp/JS Functions/JS_Functions.js
@@ -27,3 +27,17 @@ function factorial(num) {
    function kabobToSnake(str) {
     return str.replace(/-/g, "_");  /* Use a regular expression to globally locate all dashes, and replace them */
    }
+
+
+/* Send the lyrics of a song to the screen. */
+function sing() {
+    console.log("Mary had a little lamb, little lamb, little lamb,");
+    console.log("Mary had a little lamb whose fleece was white as snow!");
+}
+
+/* Higher Order functions ... functions calling or returning other functions.  */
+
+/* 'setInterval' is a native Higher Order function.  It calls another function every x milliseconds.
+   The function called as well as the interval are set from the parameters passed in. */
+var retVal = setInterval(sing, 500);  /* Calls sing() every 1/2 second, and returns it's status code to 'retVal' */
+clearInterval(retVal);              /* Stops the continuous looping of the interval.  */
