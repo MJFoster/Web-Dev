5a5a6e25b7d76183ef1c2d82dc9f3f3e89f16886
Foster <MaryJo.Foster@simplot.com>
2017-09-16 23:53:28 -0600

    Completed and Tested TodoList

--- Udemy/WebDevBootcamp/TodoList/README.txt
+++ Udemy/WebDevBootcamp/TodoList/README.txt
@@ -6,10 +6,24 @@ Page Markup ...
 <ul>
     <li>"New" - Add A ToDo</li>
     <li>"List" - View All ToDo's</li>
+    <li>"Delete" - Delete A Todo</li>
     <li>"Quit" - Quit</li>
 </ul>
 
 
 Execution Flow ...
 
-Until "quit" entered by user, prompt user "What would you like to do?"
+- Until "Quit" entered by user, prompt user "What would you like to do?"
+- When "Quit" entered by user, Alert("Thanks for playing ...").
+- Error logging and UI when invalid choices selected.
+- "New" : All todo's persist in an array, this feature adds a new todo to end of array.  Report to user after item added.
+- "List" : Display index #:, (example|  0: Buy groceries), each item on 1 line, and "**********" above and below the items.
+- "Delete" : Upon selection, 
+-       If empty, let user know and prompt again.
+-       else ...
+-           display the list and prompt user for item # to delete.
+-           report to user after item is deleted.
+- "Delete All" : Delete's the entire list in one step.
+
+
+
--- Udemy/WebDevBootcamp/TodoList/Scripts/TodoList.js
+++ Udemy/WebDevBootcamp/TodoList/Scripts/TodoList.js
@@ -1,12 +1,66 @@
-var userResponse = prompt("What would you like to do?").toUpperCase();
+var todoList = [];
+var todo = "";
+var todoIndex = -1;
+
+var userResponse = prompt("Please select an action ...").toUpperCase();
 
 while (userResponse != "QUIT") {
-    if (userResponse == "NEW") {
-        console.log("NEW entered.");
-    } else if (userResponse == "LIST") {
-        console.log("LIST entered.");
+    if (userResponse === "NEW") {
+        todo = prompt("What would you like to add to your list? ");
+        todoList.push(todo);
+        console.log(todo + " added.");
+    } else if (userResponse === "LIST") {
+        listTodos();
+    } else if (userResponse === "DELETE") {
+        deleteTodo();
+    } else if (userResponse === "DELETE ALL") {
+        deleteTodo(1);
     } else {
         console.log(userResponse + " - Invalid response entered.");
     }
-    userResponse = prompt("What would you like to do?").toUpperCase();
+    userResponse = prompt("Please select an action ...").toUpperCase();
+}
+alert("Thanks For Playing, TodoList Is Now Closed."); 
+
+function listTodos() {
+    var outputString = "";
+
+    outputString += "**********\n";
+    if (todoList.length === 0) {
+        outputString += "Todo List is empty ...\n";
+    } else {
+        todoList.forEach(function(todo,i) {
+            outputString += "(" + i + "): " + todo + "\n";
+        });        
+    }
+    outputString += "**********\n";
+    alert(outputString);
+}
+
+function deleteTodo(deleteWholeList = 0) {
+    if(todoList.length === 0) {
+        alert("There are no items to delete, please add one first.");
+    } else {
+        if(deleteWholeList != 0) {
+            todoList.splice(0,todoList.length);
+        } else {
+            listTodos();
+            todoIndex = prompt("Item # to delete OR \"Main\" to skip deleting.");
+            if (todoIndex.toUpperCase() === "MAIN") {
+                return;
+            }
+            while(isNaN(todoIndex)  ||  todoIndex < 0 || todoIndex >= todoList.length) {
+                alert("Invalid item index: " + todoIndex);
+                listTodos();
+                todoIndex = prompt("Item # to delete OR \"Main\" to skip deleting.");
+                if (todoIndex.toUpperCase() === "MAIN") {
+                    return;
+                }
+            };
+            todo = todoList[todoIndex];
+            todoList.splice(todoIndex, 1);
+            console.log("(" + todoIndex + "): " + todo + " deleted");
+        }
+    };
+    listTodos();
 }
