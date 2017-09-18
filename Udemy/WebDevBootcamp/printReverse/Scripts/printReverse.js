8b93a6f4cbfa4fde997e31a5d463dcaccaaa0e83
Foster <MaryJo.Foster@simplot.com>
2017-09-17 23:40:43 -0600

    Completed printReverse function.

--- Udemy/WebDevBootcamp/printReverse/Scripts/printReverse.js
+++ Udemy/WebDevBootcamp/printReverse/Scripts/printReverse.js
@@ -1,22 +1,36 @@
 // Print given string array to the console in REVERSE order.
 // Place old and new strings on page.
 
-var debug = 1;
+var debug = 0;
 var str = prompt("Enter String here ... ");
+var strArray = str.split("");
 var tmp;
-var j = str.length - 1;
+var newStr = "";
+var j = strArray.length - 1;
 
 if (debug) {
     console.log("Original String: " + str);
 }
 
 for(var i = 0; i < j; i++) {
-    tmp = str[i];
-    str[i] = str[j];
-    str[j--] = tmp;
+    tmp = strArray[i];
+    strArray[i] = strArray[j];
+    strArray[j--] = tmp;
+    if (debug) {
+        console.log("tmp: " + tmp);
+        console.log("String Array now: " + strArray);
+    }
 }
+
+strArray.forEach(function(ltr) {
+    newStr += ltr;
+});
 
 if (debug) {
-    console.log("New String" + str);
+    console.log("New String Array: " + strArray);
+    console.log("New String: " + newStr);
 }
 
+// Add newStr to the markup
+document.getElementById("original").innerHTML = str;
+document.getElementById("reversed").innerHTML = newStr;
