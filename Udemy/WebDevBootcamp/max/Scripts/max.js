438fc7b7351b724312c2461f6a394b2a70b2221e
Foster <MaryJo.Foster@simplot.com>
2017-09-18 23:04:18 -0600

    Completed Max() function to find maximum value of dynamically created array.

--- Udemy/WebDevBootcamp/max/Scripts/max.js
+++ Udemy/WebDevBootcamp/max/Scripts/max.js
@@ -1,3 +1,24 @@
-alert("CONNECTED - Max");
+var debug = 1;
+var numbers = [];
+var response = 'N';
+var returnString = "Array Of Numbers: ";
 
-// Returns the maximum value of given numbers array.
+while (response.toUpperCase() != 'Q') {
+    response = prompt("Enter next number for list, or 'Q' to Quit: ").toUpperCase();
+    if (response !== "Q" && !Number.isNaN(Number(response))) {  // Only push actual numbers to the numbers array
+        numbers.push(Number(response));
+        if (debug) {
+            console.log("numbers: " + numbers);
+        }
+    }
+}
+
+returnString += numbers.toString() + "\n";
+
+returnString += "Maximum Value: " + Math.max.apply(null,numbers);
+
+if (numbers.length != 0) {
+    document.getElementById("output").innerText = returnString;
+} else {
+    document.getElementById("output").innerText = "No numbers added ...";
+}
