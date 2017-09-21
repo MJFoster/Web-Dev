047ed0a757d68953b5fd660f5dd51011082a60cc
Foster <MaryJo.Foster@simplot.com>
2017-09-20 23:07:16 -0600

    Completed 'isUniform' JS

--- Udemy/WebDevBootcamp/isUniform/Scripts/isUniform.js
+++ Udemy/WebDevBootcamp/isUniform/Scripts/isUniform.js
@@ -1,3 +1,33 @@
-alert("CONNECTED to isUniform");
+/* Specifications:
+     * Boolean return that reflects if given array has all identical elements.
+     * Prompt user for csv array of data.
+     * Trim off extra spaces after ',' so that only delimiter is ','
+     * Empty array ... true
+     * Render the whole array and boolean result reflecting if array is uniform or not.
+*/
+var debug = 1;
+var elements = [];
+var allSame = true;
 
-// Boolean return that reflects if given array has all identical elements.
+elements = prompt("Enter array elements, separated by ',' (ENTER when done): ")
+            .replace(/, */g, ",")    // trim out extra spaces from delimiter
+            .split(",");
+
+if (debug) {
+    elements.forEach(function(element, i) {
+        console.log("(" + i + "): " + elements[i]);
+    })
+}
+
+for (var i = 0; i < elements.length-1 && allSame === true; i++) {
+    if (elements[i] !== elements[i+1]) {
+        allSame = false;
+    }
+}
+
+if (debug) {
+    console.log("   Uniform? " + allSame);
+}
+
+document.querySelector("#elements").textContent = elements.toString();
+document.querySelector("#isUniform").textContent = allSame;
