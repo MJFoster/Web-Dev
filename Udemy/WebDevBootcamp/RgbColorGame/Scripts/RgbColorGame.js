f93e7aa109c9f3ae313e28c78297cca55cc0c6f5
MJFoster email = mj@maryjofoster.com <MaryJo.Foster@simplot.com>
2017-11-18 17:32:27 -0500

    - Added event listeners to squares . - Added user messages for events.

--- Udemy/WebDevBootcamp/RgbColorGame/Scripts/RgbColorGame.js
+++ Udemy/WebDevBootcamp/RgbColorGame/Scripts/RgbColorGame.js
@@ -1,54 +1,63 @@
 var debug = true;
 
-var colors = [
-    "RGB(255, 0, 0)", 
-    "RGB(0, 255, 0)",
-    "RGB(0, 0, 255)",
-    "RGB(255, 255, 0)",
-    "RGB(0, 255, 255)",
-    "RGB(255, 0, 255)"
-];
+var colors = newColors();
+// var colors = [
+//     "rgb(255, 0, 0)", 
+//     "rgb(0, 255, 0)",
+//     "rgb(0, 0, 255)",
+//     "rgb(255, 255, 0)",
+//     "rgb(0, 255, 255)",
+//     "rgb(255, 0, 255)"
+// ];
 
 var squares = document.querySelectorAll(".square");
 if (debug) {
     console.log("Squares elements: " + squares);
 }
 
-// Temporarily hard code the goalColor.
-// TODO:  Randomize goalColor with 'r', 'g', 'b' values from 0-255.
-var goalColor = colors[3];
-
-displayGoalColor(goalColor);
-fillSquares();
-
-// Add a click event listener to each square.
-squares.forEach(function(square) {
-    square.addEventListener("click", function() {
-        if(this.style.backgroundColor === goalColor) {
-            /* Update colors[] */
-            colors.forEach(function(color, index) {
-
-            });
-            fillSquares();
-            /* update banner backgroundColor */
-            /* Add 'Try Again' or 'Correct' message on menu bar */
-            /* Add text 'New Colors' / 'Play Again?' to menu bar */
-        } else {
-            /* addClass 'dark-background' to clicked square. */
-            /* Show 'Try Again' centered between 'NEW COLORS' and 'EASY' */
-        }
-    })
-});
+// Set goalColor by randomly choosing from colors[].
+var goalColor = colors[Math.floor(Math.random() * colors.length)];
 
 
-/* Fill all squares with colors currently in colors[] */
-function fillSquares() {
+displayRgbGoalColor(goalColor);
+InitializeSquares();
+
+/* Color and add event listeners to squares */
+function InitializeSquares() {
     squares.forEach(function(square, index) {
         square.style.backgroundColor = colors[index];
+        // Add an event listener to each square that updates background color(s) when clicked.
+        square.addEventListener("click", function() {
+            var clickedColor = this.style.backgroundColor;
+            if(clickedColor === goalColor) {
+                /* TODO Update all squares & banner background color to goalColor, update user messages & buttons*/
+                matchAllSquares(clickedColor);
+                document.querySelector("#banner").style.backgroundColor = clickedColor;
+                document.querySelector("#user-message").innerHTML = "Correct!";
+                /* TODO Update buttons 'New Colors' / 'Play Again?' to menu bar */
+            } else {
+                // hide clicked square, and update #user-message.
+                this.style.backgroundColor = "#232323";
+                document.querySelector("#user-message").innerHTML = "Try Again!";
+            }
+        });
     });
 }
 
 /* Update the "RGB" text in bannaer.  */
-function displayGoalColor (color) {
+function displayRgbGoalColor (color) {
     document.querySelector("#rgb-color").innerHTML = color;
+}
+
+/* Match all squares[] to given color */
+function matchAllSquares(newColor) {
+    squares.forEach(function(square) {
+        square.style.backgroundColor = newColor;
+    })
+}
+
+/* Randomly determine colors */
+function newColors() {
+// TODO Randomize squares[] with r-g-b values from 0-255 each
+    ;
 }
